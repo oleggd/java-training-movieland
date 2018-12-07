0eaba4f80224ac2fc89defc8f791b32bd356b35a
olegg <olegg.dov@gmail.com>
2018-12-07 11:08:30 +0200

    Java trainings - Movieland [b-5] - sonar fixes

--- src/main/java/com/movieland/controller/GenreController.java
+++ src/main/java/com/movieland/controller/GenreController.java
@@ -7,8 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
